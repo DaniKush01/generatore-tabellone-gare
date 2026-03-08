cdace814d34bb39840f074044ccead741022a50b
DaniKush01 <angeli.daniele01@gmail.com>
2026-03-08 15:58:24 +0100

    Update database.php

--- config/database.php
+++ config/database.php
@@ -1,1 +1,11 @@
+<?php
 
+return [
+
+    'host' => '127.0.0.1',
+    'db'   => 'u418740807_ea0OF',
+    'user' => 'u418740807_QO934',
+    'pass' => 'INSERISCI_PASSWORD',
+    'charset' => 'utf8mb4'
+
+];
