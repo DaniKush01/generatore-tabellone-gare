39fb97fc7b86db929df62bd0e623c7ce0b47d93f
DaniKush01 <angeli.daniele01@gmail.com>
2026-03-08 16:53:40 +0100

    Update admin_only.js

--- js/admin_only.js
+++ js/admin_only.js
@@ -3,10 +3,10 @@
 */
 (function () {
 
-  // controllo ruolo
   const isAdmin = sessionStorage.getItem('isAdmin') === 'true';
+
   if (!isAdmin) {
-      // puoi cambiare destinazione o mostrare un messaggio a piacere
       window.location.href = 'menu.html';
   }
+
 })();
