78150e1c8a72eb44821679494ca2d8aea5dd616b
DaniKush01 <angeli.daniele01@gmail.com>
2026-03-08 16:30:43 +0100

    Update config_public.php

--- config/config_public.php
+++ config/config_public.php
@@ -1,1 +1,12 @@
+<?php
 
+header('Content-Type: application/json; charset=utf-8');
+
+$config = require __DIR__.'/config/app_config.php';
+
+echo json_encode([
+    'brand' => $config['brand'],
+    'theme' => $config['theme'],
+    'competitions' => $config['competitions'],
+    'weight_classes' => $config['weight_classes']
+], JSON_UNESCAPED_UNICODE);
